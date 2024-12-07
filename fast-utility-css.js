f36e52e2c823bcdfbcfa9dffa495c66aa813007a
Neil Mix <neilmix@gmail.com>
2024-12-06 23:18:07 -0600

    New naming scheme. Added ability to pass-through values that haven't been defined.

--- fast-utility-css.js
+++ fast-utility-css.js
@@ -1,146 +1,157 @@
 (() => {
+  const word_abr_str = 
+`b:bottom c:color d:display h:height l:left m:margin p:padding r:right t:top w:width
+al:align bg:background bs:basis br:border bk:break cl:column ct:content cs:cursor dc:decoration dr:direction fl:flex fm:family fn:font gp:gap gr:grow it:items js:justify ln:line mx:max mn:min op:opacity of:overflow ps:position rd:radius rw:row sf:self sh:shrink sz:size sp:space st:style tx:text tr:transform vs:visibility vt:vertical wt:weight wh:white wr:wrap x:x y:y
+abs:absolute arn:around aut:auto bal:balance bet:between blk:block bld:bold bdr:bolder bsl:baseline cap:capitalize cps:collapse ctr:center def:default dot:dotted dsh:dashed end:end evn:evenly fxd:fixed hid:hidden inl:inline itc:italic low:lowercase ltr:lighter mid:middle no:no nrm:normal obq:olblique ptr:pointer pre:pre rel:relative scr:scroll sol:solid srt:start stb:stable stt:static stk:sticky str:stretch sub:sub sup:super thr:through und:underline upp:uppercase wr:wrap`;
+
+  const valid_props_str = 
+`alct:nrm,str,srt,r,end,flsrt,flend,bsl,spbet,sparn,spevn
+alit:nrm,str,srt,ctr,end,flsrt,flend,bsl,sfsrt,sfend
+alsf:aut,nrm,str,srt,ctr,end,flsrt,flend,bsl,sfsrt,sfend
+jsct:nrm,srt,r,end,flsrt,flend,l,r,spbet,sparn,spevn,str
+jsit:nrm,srt,ctr,end,flsrt,flend,sfsrt,sfend,l,r,bsl,str
+jssf:aut,nrm,srt,ctr,end,flsrt,flend,sfsrt,sfend,l,r,bsl,str
+txal:srt,end,l,r,ctr,js
+brst:no,hid,sol,dsh,dot
+brstt:no,hid,sol,dsh,dot
+brstr:no,hid,sol,dsh,dot
+brstb:no,hid,sol,dsh,dot
+brstl:no,hid,sol,dsh,dot
+brsthz:no,hid,sol,dsh,dot
+brstvt:no,hid,sol,dsh,dot
+cs:aut,def,no,ptr
+d:no,blk,fl,inl,inlblk,inlfl
+fldr:rw,cl
+flwr:,no,wr
+fnst:nrm,itc,obq
+fnwt:nrm,bld,ltr,bdr
+of:vs,hid,scr,aut
+ps:stt,rel,abs,fxd,stk
+txdc:no,und,lnthr
+txtr:no,cap,upp,low
+vtal:bsl,sub,sup,txt,txb,mid,alt,alb
+vs:,vs,hid,cps
+whsp:nrm,no,pre,preln,prewr,bksp`;
+
+  const word_lookup = word_abr_str.split(/[ \n]+/).reduce((o, cfg) => {
+    const [key, value] = cfg.split(":");
+    o[key] = value;
+    return o;
+  }, {});
+
+  function expand_name(abr) {
+    let i = 0;
+    let prop = '';
+    while (i < abr.length) {
+      let sym = abr.slice(i, i + 2);
+      if (!word_lookup[sym]) sym = abr[i];
+
+      let word = word_lookup[sym];
+      if (!word) break;
+
+      if (prop) prop += "-";
+      prop += word;
+      i += sym.length;
+    }
+    if (i < abr.length) throw new Error(`Unknown property: ${abr}`);
+    return prop;
+  }
+
+  function expand_value(abr) {
+    let i = 0;
+    let val = '';
+    while (i < abr.length) {
+      let sym = abr.slice(i, i + 3);
+      if (!word_lookup[sym]) sym = abr.slice(i, i + 2);
+      if (!word_lookup[sym]) sym = abr[i];
+
+      let word = word_lookup[sym];
+      if (!word) break;
+
+      if (word == "visibility") word = "visible";
+
+      if (val) val += "-";
+      val += word;
+      i += sym.length;
+    }
+
+    if (i < abr.length) throw new Error(`Unknown value: ${abr}`);
+    return val;
+  }
+
+  let conv_sheet = '';
+  const valid_props_arr = valid_props_str.split(/[\n:]/);
+  let statics = {};
+  for (let i = 0; i < valid_props_arr.length; i += 2) {
+    const name = valid_props_arr[i];
+    const values = valid_props_arr[i + 1].split(",");
+    if (name.endsWith("hz")) {
+      for (let value of values) {
+        const short = name.slice(0, -2);
+        const rulename = `${name}${value}`;
+        const def = `${statics[`${short}l${value}`]} ${statics[`${short}r${value}`]};`;
+        statics[rulename] = def;
+        conv_sheet += `.${rulename} { ${def} }\n`;
+      }
+      continue;
+    }
+    if (name.endsWith("vt")) {
+      for (let value of values) {
+        const short = name.slice(0, -2);
+        const rulename = `${name}${value}`;
+        const def = `${statics[`${short}t${value}`]} ${statics[`${short}b${value}`]};`;
+        statics[rulename] = def;
+        conv_sheet += `.${rulename} { ${def} }\n`;
+      }
+      continue;
+    }
+    for (let value of values) {
+      const name_ex = expand_name(name);
+      let value_ex;
+      if (value == "no") {
+        if (["brst", "brstt", "brstr", "brstb", "brstl", "cs", "d", "txdc", "txtr"].includes(name)) {
+          value_ex = "none";
+        } else if (["flwr", "whsp"].includes(name)) {
+          value_ex = "nowrap";
+        } else {
+          throw new Error(`Can't find value "no" for ${name_ex}`);
+        }
+      } else {
+        value_ex = expand_value(value);
+        if (value == "bksp") value_ex += "s";
+        if (value_ex == "") value_ex = { "flwr": "wrap", "vs": "visible" }[name];
+      }
+      const rulename = `${name}${value}`;
+      const def = `${name_ex}: ${value_ex};`;
+      statics[rulename] = def;
+      conv_sheet += `.${rulename} { ${def} }\n`;
+    }
+  }
+  
   const el = document.createElement("style");
-  const statics = {
-    // display
-    nodisplay: "display: none;",
-    block: "display: block;",
-    flex: "display: flex;",
-
-    // visibility
-    hidden: "visibility: hidden;",
-    visible: "visibility: visible;",
-
-    // position
-    relative: "position: relative;",
-    absolute: "position: absolute;",
-    fixed: "position: fixed;",
-    sticky: "position: sticky;",
-
-    // flex
-    col: "flex-direction: column;",
-    row: "flex-direction: row;",
-    colr: "flex-direction: column-reverse;",
-    rowr: "flex-direction: row-reverse;",
-    wrap: "flex-wrap: wrap;",
-    wrapn: "flex-wrap: nowrap;",
-    wrapr: "flex-wrap: wrap-reverse;",
-    flex1: "flex: 1;",
-    grow0: "flex-grow: 0;",
-    grow1: "flex-grow: 1;",
-    grow0: "flex-grow: 0;",
-    grow1: "flex-grow: 1;",
-    grow2: "flex-grow: 2;",
-    grow3: "flex-grow: 3;",
-    grow4: "flex-grow: 4;",
-    grow5: "flex-grow: 5;",
-    shrink0: "flex-shrink: 0;",
-    shrink1: "flex-shrink: 1;",
-    shrink2: "flex-shrink: 2;",
-    shrink3: "flex-shrink: 3;",
-    shrink4: "flex-shrink: 4;",
-    shrink5: "flex-shrink: 5;",
-    jstart: "justify-content: flex-start;",
-    jcenter: "justify-content: center;",
-    jend: "justify-content: flex-end;",
-    jbetween: "justify-content: space-between;",
-    jaround: "justify-content: space-around;",
-    jevenly: "justify-content: space-evenly;",
-    istart: "align-items: flex-start;",
-    icenter: "align-items: center;",
-    iend: "align-items: flex-end;",
-    istretch: "align-items: stretch;",
-    ibaseline: "align-items: baseline;",
-    slfstart: "align-self: flex-start;",
-    slfcenter: "align-self: center;",
-    slfend: "align-self: flex-end;",
-    slfstretch: "align-self: stretch;",
-    slfbaseline: "align-self: baseline;",
-    cstart: "align-content: flex-start;",
-    ccenter: "align-content: center;",
-    cend: "align-content: flex-end;",
-    cstretch: "align-content: stretch;",
-    cbetween: "align-content: space-between;",
-    caround: "align-content: space-around;",
-
-    // overflow
-    ofhidden: "overflow: hidden;",
-    ofvisible: "overflow: visible;",
-    ofscroll: "overflow: scroll;",
-    ofauto: "overflow: auto;",
-    ofxhidden: "overflow-x: hidden;",
-    ofxvisible: "overflow-x: visible;",
-    ofxscroll: "overflow-x: scroll;",
-    ofxauto: "overflow-x: auto;",
-    ofyhidden: "overflow-y: hidden;",
-    ofyvisible: "overflow-y: visible;",
-    ofyscroll: "overflow-y: scroll;",
-    ofyauto: "overflow-y: auto;",
-
-    // borders
-    solid: "border-style: solid;",
-    dotted: "border-style: dotted;",
-    dashed: "border-style: dashed;",
-    double: "border-style: double;",
-    groove: "border-style: groove;",
-    ridge: "border-style: ridge;",
-    inset: "border-style: inset;",
-    outset: "border-style: outset;",
-
-    // text
-    uppercase: "text-transform: uppercase;",
-    lowercase: "text-transform: lowercase;",
-    capitalize: "text-transform: capitalize;",
-    plain: "text-decoration: none;",
-    underline: "text-decoration: underline;",
-    overline: "text-decoration: overline;",
-    line: "text-decoration: line-through;",
-    bold: "font-weight: bold;",
-    bolder: "font-weight: bolder;",
-    lighter: "font-weight: lighter;",
-    normal: "font-weight: normal; font-style: normal;",
-    italic: "font-style: italic;",
-    oblique: "font-style: oblique;",
-
-    // cursor
-    pointer: "cursor: pointer;",
-    default: "cursor: default;",
-    text: "cursor: text;",
-    move: "cursor: move;",
-    notallowed: "cursor: not-allowed;",
-
-    // other
-    opacity0: "opacity: 0;",
-    opacity1: "opacity: 1;",
-  };
   el.textContent = `
-    /* reset */
-    html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video { margin: 0; padding: 0; border: 0; font-size: 100%; font: inherit; vertical-align: baseline; }
-    article, aside, details, figcaption, figure, footer, header, hgroup, menu, nav, section { display: block; }
-    body { line-height: 1; }
-    ol, ul { list-style: none; }
-    blockquote, q { quotes: none; }
-    blockquote:before, blockquote:after,
-    q:before, q:after { content: ''; content: none; }
-    table { border-collapse: collapse; border-spacing: 0; }
-    * { border-width: 0px; }
-    * { box-sizing: border-box; }
-
-    ${Object.entries(statics)
-      .map(([key, value]) => `.${key} { ${value} }`)
-      .join("\n")}
+  /* reset */
+  html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video { margin: 0; padding: 0; border: 0; font-size: 100%; font: inherit; }
+  body { line-height: 1; }
+  ol, ul { list-style: none; }
+  blockquote, q { quotes: none; }
+  blockquote:before, blockquote:after, q:before, q:after { content: ''; content: none; }
+  table { border-collapse: collapse; border-spacing: 0; }
+  * { border-width: 0px; }
+  * { box-sizing: border-box; }
+  ${conv_sheet}
   `;
-
   document.head.appendChild(el);
   const sheet = document.styleSheets[document.styleSheets.length - 1];
 
   const vars = {
     color: {},
     size: {},
-    width: {},
-    height: {},
-    spacing: {},
-    position: {},
-    radius: {},
+    family: {},
+    dim: {},
+    space: {},
+    pos: {},
+    number: {},
   };
 
   const propertyRx = new RegExp(`^--(${Object.keys(vars).join("|")})-(.+)$`);
@@ -185,74 +196,90 @@
 
   const propMap = {
     color: {
-      txt: ["color"],
-      bg: ["background-color"],
-      brd: ["border-color"],
-      brdt: ["border-top-color"],
-      brdr: ["border-right-color"],
-      brdb: ["border-bottom-color"],
-      brdl: ["border-left-color"],
-      brdh: ["border-left-color", "border-right-color"],
-      brdv: ["border-top-color", "border-bottom-color"],
+      c: ["color"],
+      bgc: ["background-color"],
+      brc: ["border-color"],
+      brtc: ["border-top-color"],
+      brrc: ["border-right-color"],
+      brbc: ["border-bottom-color"],
+      brlc: ["border-left-color"],
+      brhzc: ["border-left-color", "border-right-color"],
+      brvtc: ["border-top-color", "border-bottom-color"],
+      tdc: ["text-decoration-color"],
     },
     size: {
-      txt: ["font-size"],
+      fnsz: ["font-size"],
+      lnh: ["line-height"],
     },
-    width: {
+    family: {
+      fnfm: ["font-family"],
+    },
+    dim: {
       w: ["width"],
+      mnw: ["min-width"],
+      mxw: ["max-width"],
       wmn: ["min-width"],
       wmx: ["max-width"],
-      brd: ["border-width"],
-      brdt: ["border-top-width"],
-      brdr: ["border-right-width"],
-      brdb: ["border-bottom-width"],
-      brdl: ["border-left-width"],
-      brdh: ["border-left-width", "border-right-width"],
-      brdv: ["border-top-width", "border-bottom-width"],
-      basis: ["flex-basis"],
-    },
-    height: {
       h: ["height"],
+      mnh: ["min-height"],
+      mxh: ["max-height"],
       hmn: ["min-height"],
       hmx: ["max-height"],
+      flbs: ["flex-basis"],
     },
-    spacing: {
+    space: {
+      brw: ["border-width"],
+      brtw: ["border-top-width"],
+      brrw: ["border-right-width"],
+      brbw: ["border-bottom-width"],
+      brlw: ["border-left-width"],
+      brhzw: ["border-left-width", "border-right-width"],
+      brvtw: ["border-top-width", "border-bottom-width"],
+      brrd: ["border-radius"],
+      brtlrd: ["border-top-left-radius"],
+      brtrrd: ["border-top-right-radius"],
+      brbrrd: ["border-bottom-right-radius"],
+      brblrd: ["border-bottom-left-radius"],
+      brtrd: ["border-top-left-radius", "border-top-right-radius"],
+      brrrd: ["border-top-right-radius", "border-bottom-right-radius"],
+      brbrd: ["border-bottom-right-radius", "border-bottom-left-radius"],
+      brlrd: ["border-bottom-left-radius", "border-top-left-radius"],
       m: ["margin"],
       mt: ["margin-top"],
       mr: ["margin-right"],
       mb: ["margin-bottom"],
       ml: ["margin-left"],
-      mh: ["margin-left", "margin-right"],
-      mv: ["margin-top", "margin-bottom"],
+      mhz: ["margin-left", "margin-right"],
+      mvt: ["margin-top", "margin-bottom"],
       p: ["padding"],
       pt: ["padding-top"],
       pr: ["padding-right"],
       pb: ["padding-bottom"],
       pl: ["padding-left"],
-      ph: ["padding-left", "padding-right"],
-      pv: ["padding-top", "padding-bottom"],
+      phz: ["padding-left", "padding-right"],
+      pvt: ["padding-top", "padding-bottom"],
       gp: ["gap"],
-      gpr: ["row-gap"],
-      gpc: ["column-gap"],
+      gprw: ["row-gap"],
+      gpcl: ["column-gap"],
+      rwgp: ["row-gap"],
+      clgp: ["column-gap"],
     },
-    position: {
-      post: ["top"],
-      posr: ["right"],
-      posb: ["bottom"],
-      posl: ["left"],
+    pos: {
+      t: ["top"],
+      r: ["right"],
+      b: ["bottom"],
+      l: ["left"],
     },
-    radius: {
-      radius: ["border-radius"],
-      radiustl: ["border-top-left-radius"],
-      radiustr: ["border-top-right-radius"],
-      radiusbr: ["border-bottom-right-radius"],
-      radiusbl: ["border-bottom-left-radius"],
-    },
+    number: {
+      flgr: ["flex-grow"],
+      flsh: ["flex-shrink"],
+      op: ["opacity"],
+    }
   };
   const rules = {};
   function addStyles(elements) {
-    for (const element of elements) {
-      for (const className of element.classList) {
+    for (const el of elements) {
+      for (const className of el.classList) {
         if (rules[className]) continue;
         if (className.match(/[:\-\.]/)) {
           insertRule(className);
@@ -287,12 +314,14 @@
 
     const [, prop, varname] = identifier.match(/^([^-]+)-(.+)$/) || [];
     for (context in propMap) {
-      const value = vars[context][varname];
-      if (!value) continue;
+      let value = vars[context][varname];
+      // if we don't find a matching var, use the varname as the value which allows people to put
+      // in values on-the-fly (e.g. w-400px)
+      if (!value) value = varname;
       const propsText = (propMap[context][prop] || [])
         .map((propName) => `${propName}:${value};`)
         .join(" ");
-      sheet.insertRule(`${selector} { ${propsText} }`, sheet.cssRules.length);
+        sheet.insertRule(`${selector} { ${propsText} }`, sheet.cssRules.length);
     }
   }
 })();
